93b80cfbf6e25ea0ee8dfdd39b4bfb031a2191d1
Chaz Carbis <chazcarbis@gmail.com>
2021-07-09 15:59:16 -0500

    done with /projects endpoints

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -2,7 +2,7 @@
 const Projects = require('./projects-model')
 
 function logger(req, res, next) {
-    const timestamp = new Date().toLocaleString
+    const timestamp = new Date().toLocaleString()
     const method = req.method
     const url = req.originalUrl
     console.log(`${timestamp} ${method} ${url}`)
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -25,7 +25,30 @@ router.post('/', validateProject, (req, res, next) => {
 })
 
 router.put('/:id', validateProject, validateProjectId, (req, res, next) => {
-    Projects.update
+    Projects.update(req.params.id, req.project)
+        .then((updatedProject) => {
+            res.json(updatedProject)
+        })
+        .catch(next)
 })
 
+router.delete('/:id', validateProjectId, async(req, res, next) => {
+    try {
+        await Projects.remove(req.params.id)
+        res.json(req.project)
+    } catch (error) {
+        next(error)
+    }
+
+})
+
+router.get("/:id/actions", validateProjectId, async(req, res, next) => {
+    try {
+        const result = await Projects.getProjectActions(req.params.id)
+        res.json(result)
+        console.log(result)
+    } catch (error) {
+        next(error)
+    }
+})
 module.exports = router
